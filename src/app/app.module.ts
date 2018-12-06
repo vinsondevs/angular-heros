a538fd06341f6bb19b114f4934bb996a13a96d9b
Vinson A <vinson.a@kqics.com>
2018-12-06 15:31:15 +0530

    footer component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
